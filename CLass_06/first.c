2ab8610205b3bd15d657a5b47bf001debb5e7006
Agil444 <taqilet@gmail.com>
2022-09-28 13:51:32 +0300

    Class_06

---
+++ CLass_06/first.c
@@ -0,0 +1,12 @@
+//comment of one row
+//sometig 
+
+/*
+commenting a paragraph or code fragment
+*/
+int  main() // for row comment tghere is only // smb in beginning of comment 
+ {
+ // return 100;
+  return 0;
+ }
+
