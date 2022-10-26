705de38580e44fbe0dd28a5ce6a60a5c8aa5e7d9
Agil444 <113023920+Agil444@users.noreply.github.com>
2022-10-26 13:47:42 +0300

    Create loop_do_while_test.c

---
+++ Class12/loop_do_while_test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+ {
+ char number = 25;
+
+ do
+  {
+  printf("c = %d\n",number);
+  number--;
+  }
+ while(number>0);
+
+ printf("Print after while loop.\n");
+
+ return 0;
+ }
