06c1771a5978f29aadcd2a8c7e7b5a49bc048240
Agil444 <113023920+Agil444@users.noreply.github.com>
2022-10-26 13:52:03 +0300

    Update loop_for_tests.c

--- Class12/loop_for_tests.c
+++ Class12/loop_for_tests.c
@@ -2,15 +2,15 @@
 
 int main()
  {
- char c;
+ char A;
 
- for(c=10 ; c>=0 ; c--) // part before first ; is executed only one time
+ for(A=10 ; A>=0 ; c++) // part before first ; is executed only one time
                         // part before first ; can contain multiple actions follwed by comma
                         // for(action_a,action_b ; expression ; action_y, action_z)
                         // part after second ; is executed multiple times
                         // execution of action(s) of loop continues while expression is true
   {
-  printf("c = %d\n",c);
+  printf("A = %d\n",A);
   }
 
  printf("Print after for loop.\n");
