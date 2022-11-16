33f2c82442eb1a99683fb21c0231d647b577a880
Agil444 <113023920+Agil444@users.noreply.github.com>
2022-11-16 15:11:30 +0200

    Update check_test.c

--- check_test.c
+++ check_test.c
@@ -1,9 +1,53 @@
 #include <stdio.h>
-int main(void) 
+#include <string.h>
+#include <stdbool.h>
+#define N 100
+void print_occurrences(char *s);
+
+int main()
 {
-char name [] = "Agil"
-  printf ("%c", *(name+1));
-  char *namePtr;
-  namePtr = name;
-  printf("%c", *namePtr);
+  // test the function out with a test string
+  char sentence[N];
+  printf("Enter sentence:\n");
+  //scanf("%s", sentence);
+  fgets(sentence, N-1,stdin);
+  print_occurrences(sentence);
+
+  return 0;
+}
+
+// prints the number of occurrences of each unique character in the string s
+void print_occurrences(char *s)
+{
+  // unique will keep track of the unique characters found so far, counted 
+  // will keep track of how many unique characters we've already counted
+  int length = strlen(s);
+  //char unique[length];
+  //int counted = 0;
+  
+  // check each character in the string
+  for (int i = 0; i < length; i++)
+  {
+    // check to see if the character at index i has already been counted
+    //bool already_counted = false;
+   // for (int j = 0; j < counted; j++)
+     // if (s[i] == unique[j])
+      //  already_counted = true;
+    
+    // if the character has already been counted, skip ahead to the next 
+    // iteration of the loop as we do not need to count it again
+   // if (already_counted) continue;
+    
+    // if we haven't already counted the character, count it now
+    int count = 0;
+    for (int j = 0; j < length; j++)
+      if (s[i] == s[j]) count++;
+    
+    // output the count
+    printf("%c - %d\n", s[i], count);
+    
+    // add the character to the array of unique characters we've found so far
+   // unique[counted] = s[i];
+  //counted++;
+  }
 }
